0b815146e746f53a4ccd8287f22c2e947c6022c7
abdurasul29052002 <abdurasulabduraimov22@gmail.com>
2025-05-23 16:31:46 +0500

    added telegram bot initializer

---
+++ src/main/java/uz/sonic/telegrambots/component/TelegramBotInitializer.java
@@ -0,0 +1,27 @@
+package uz.sonic.telegrambots.component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class TelegramBotInitializer {
+    private final AbstractTelegramBot telegramBot;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void init(){
+        try {
+            var telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(telegramBot);
+            log.info("Bot {} registered successfully", telegramBot.getBotUsername());
+        } catch (Exception e) {
+            log.warn("Could not register bot: {}", e.getMessage());
+        }
+    }
+}
